4d9234516cde8252dd943c129d88df8098c00412
Shane McElroy <mcelroysh@gmail.com>
2025-07-02 18:24:17 -0700

    Set up hover effect on DIVs
    Grid DIVs change color when the mouse hovers over them,
    leaving a trail through the grid like a pen would.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 const container = document.querySelector(".container");
 const GRIDSIZE = 16;
 
+createGrid(GRIDSIZE);
+
+container.addEventListener("mouseover", (event) => {
+  if (event.target.classList.contains("div-item")) {
+    event.target.classList.toggle("change-color");
+  }
+});
+
 function createGrid(size) {
+  // Create columns
   for (let i = 0; i < size; i++) {
     let column = document.createElement("div");
+    // Create rows
     for (let j = 0; j < size; j++) {
       let row = document.createElement("div");
+      row.classList.add("div-item");
       // row.textContent = i * size + j;
       column.appendChild(row);
     }
     container.appendChild(column);
   }
 }
-
-createGrid(GRIDSIZE);
